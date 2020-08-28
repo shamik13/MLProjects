225402cedd41db66c5141b7a59c16e6e3525a69c
TaikiInoue <taikiinoue45@gmail.com>
2020-08-28 13:02:34 +0900

    Initial commit

--- sandbox/entrypoint.sh
+++ sandbox/entrypoint.sh
@@ -1,8 +1,12 @@
+sed -i "s/console, file/file/" /opt/conda/lib/python3.6/site-packages/hydra/conf/hydra/job_logging/default.yaml
+mkdir -p /dgx/data
+sshfs inoue.momo:/dgx/inoue/data/ /dgx/data
+
+mkdir -p /dgx/github
+cd /dgx/github
 git clone https://github.com/TaikiInoue/SSAD.git
-cd SSAD
+cd /dgx/github/SSAD
 git checkout master
 git checkout 31c664b53f14719a2f1623e83af7ad7e366be194
-cd /
-tree
-ls
-pwd
+
+python ssad/run.py ssad/conf/config.yaml
