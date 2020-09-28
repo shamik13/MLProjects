a13cb017cc5ebc7f926da3a033c2ee0acea422fc
Shamik Kundu <shamikkundu13@gmail.com>
2020-09-28 17:32:23 +0900

    Update docker.sh

--- somic/code/docker.sh
+++ somic/code/docker.sh
@@ -1,2 +1,2 @@
 docker build -t somic .
-MLFLOW_EXPERIMENT_NAME='/Users/shamik@nablas.com/airflow_test' mlflow run  .
+MLFLOW_EXPERIMENT_NAME='/Users/shamik@nablas.com/airflow_test' mlflow run --docker-args gpus=all .
